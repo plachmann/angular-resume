a01a2ceb3302508f088ad72e1e39544c12c46721
Phil Lachmann <phil.lachmann@gmail.com>
2018-12-30 10:31:52 -0500

    base component with bootstrap and fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './components/education/education.component';
+import { WorkHistoryComponent } from './components/work-history/work-history.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    WorkHistoryComponent,
+    SkillsComponent,
+    AboutMeComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
